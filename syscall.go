f14d347e76116edaab0a971ed84463a27d38b5b3
Hajime Hoshi <hajimehoshi@gmail.com>
2022-06-04 17:59:42 +0900

    add go:nosplit to syscall9X

--- syscall.go
+++ syscall.go
@@ -48,6 +48,7 @@ var syscall9XABI0 uintptr
 
 func syscall9X() // implemented in assembly
 
+//go:nosplit
 func syscall_syscall9X(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9 uintptr) (r1, r2, err uintptr) {
 	args := struct{ fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, r1, r2, err uintptr }{
 		fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, r1, r2, err}
