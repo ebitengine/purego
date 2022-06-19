e18209d81ada5ad768937c5ea9b6e8c0a236d87c
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-06-18 22:24:45 -0400

    purego: add //go:uintptrescapes to SyscallN
    this forces the uintptr arguments to escape in SyscallN. When optimizations were enabled Go assumed that the pointer arguments in SyscallN were no longer needed. This is why functions like gl.GenFramebuffersEXT would fail. Although this change forces all of the arguments to be placed on the heap it ensures that no optimizations will remove our precious arguments. This also allows us to only have one implementation inside SyscallN (i.e. syscall_syscall9X). A potential replacement would be //go:cgo_unsafe_args but that can only be used in CGO generated files. There is also //go:uintptrkeepalive although that was not enough to stop the function from failing. That is why I went with //go:uintptrescapes
    
    I tested this change on darwin (arm64 & amd64) as well as with -gcflags=all=-N and without. All tests pass on these platforms.
    
    I have not tested this on windows. If you could that would be much appreciated.

--- syscall.go
+++ syscall.go
@@ -8,20 +8,6 @@ package purego
 
 const maxArgs = 9
 
-// f matches argument numbers to a Syscall implementation that can take that many
-var f = map[int]func(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9 uintptr) (r1, r2, err uintptr){
-	0: syscall_syscallX3,
-	1: syscall_syscallX3,
-	2: syscall_syscallX3,
-	3: syscall_syscallX3,
-	4: syscall_syscallX6,
-	5: syscall_syscallX6,
-	6: syscall_syscallX6,
-	7: syscall_syscall9X,
-	8: syscall_syscall9X,
-	9: syscall_syscall9X,
-}
-
 // SyscallN takes fn, a C function pointer and a list of arguments as uintptr.
 // There is an internal maximum number of arguments that SyscallN can take. It panics
 // when the maximum is exceeded. It returns the result and the libc error code if there is one.
@@ -33,6 +19,7 @@ var f = map[int]func(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9 uintptr) (r1, r2, er
 // On amd64, if there are more than 8 floats the 9th and so on will be placed incorrectly on the
 // stack.
 //go:nosplit
+//go:uintptrescapes
 func SyscallN(fn uintptr, args ...uintptr) (r1, r2, err uintptr) {
 	if len(args) > maxArgs {
 		panic("too many arguments to SyscallN")
@@ -40,5 +27,5 @@ func SyscallN(fn uintptr, args ...uintptr) (r1, r2, err uintptr) {
 	// add padding so there is no out-of-bounds slicing
 	var tmp [maxArgs]uintptr
 	copy(tmp[:], args)
-	return f[len(args)](fn, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6], tmp[7], tmp[8])
+	return syscall_syscall9X(fn, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6], tmp[7], tmp[8])
 }
