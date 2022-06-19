bda3acfed6e93f77ef1418d21a2bc2012a58fbfd
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-06-18 23:41:57 -0400

    purego: add //go:uintptrescapes to SyscallN (#7)
    this forces the uintptr arguments to escape in SyscallN. When optimizations were enabled Go assumed that the pointer arguments in SyscallN were no longer needed. This is why functions like gl.GenFramebuffersEXT would fail. Although this change forces all of the arguments to be placed on the heap it ensures that no optimizations will remove our precious arguments. This also allows us to only have one implementation inside SyscallN (i.e. syscall_syscall9X). A potential replacement would be //go:cgo_unsafe_args but that can only be used in CGO generated files. There is also //go:uintptrkeepalive although that was not enough to stop the function from failing. That is why I went with //go:uintptrescapes
    
    I tested this change on darwin (arm64 & amd64) as well as with -gcflags=all=-N and without. All tests pass on these platforms.
    
    I have not tested this on windows. If you could that would be much appreciated.

--- syscall_darwin.go
+++ syscall_darwin.go
@@ -8,16 +8,6 @@ func callc(fn uintptr, args unsafe.Pointer) {
 	runtime_exitsyscall()
 }
 
-//go:nosplit
-func syscall_syscallX3(fn, a1, a2, a3, _, _, _, _, _, _ uintptr) (r1, r2, err uintptr) {
-	return syscall_syscall6X(fn, a1, a2, a3, 0, 0, 0)
-}
-
-//go:nosplit
-func syscall_syscallX6(fn, a1, a2, a3, a4, a5, a6, _, _, _ uintptr) (r1, r2, err uintptr) {
-	return syscall_syscall6X(fn, a1, a2, a3, a4, a5, a6)
-}
-
 var syscall9XABI0 uintptr
 
 func syscall9X() // implemented in assembly
