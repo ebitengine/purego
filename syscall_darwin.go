20f543febac63378870c890e1c17bf33a3761c6e
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-07-06 22:10:30 -0400

    add symbols to c functions

--- syscall_darwin.go
+++ syscall_darwin.go
@@ -7,12 +7,6 @@ import (
 	"unsafe"
 )
 
-func callc(fn uintptr, args unsafe.Pointer) {
-	runtime_entersyscall()
-	runtime_libcCall(fn, args)
-	runtime_exitsyscall()
-}
-
 var syscall9XABI0 uintptr
 
 type syscall9Args struct{ fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, r1, r2, err uintptr }
@@ -20,7 +14,7 @@ type syscall9Args struct{ fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, r1, r2, err ui
 //go:nosplit
 func syscall_syscall9X(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9 uintptr) (r1, r2, err uintptr) {
 	args := syscall9Args{fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, r1, r2, err}
-	callc(syscall9XABI0, unsafe.Pointer(&args))
+	runtime_cgocall(syscall9XABI0, unsafe.Pointer(&args))
 	return args.r1, args.r2, args.err
 }
 
