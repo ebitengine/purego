57b4defe75d00dd37fffe98e98a554221ec83dd2
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-07-06 22:27:33 -0400

    remove doc.go

--- objc/doc.go
+++
@@ -1,7 +0,0 @@
-// Package objc is a low-level pure Go objective-c runtime. This package is easy to use incorrectly so it is best
-// to use a wrapper that provides the functionality you need in a safer way.
-//
-// All functions that take a string as an argument are NULL terminated ('\x00'). This is so that there is no
-// need to copy the string and put pressure on the GC. The decision to go this route is because objective-c
-// calls into the runtime a lot and there would be a lot of time wasted just copying strings around.
-package objc
