04e3be7a5f0dd2a7372a7b381caa6bff08236620
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-07-20 16:12:55 -0400

    revert changes back to not using cgocall

--- go_runtime.go
+++ go_runtime.go
@@ -10,5 +10,9 @@ import (
 	"unsafe"
 )
 
-//go:linkname runtime_cgocall runtime.cgocall
-func runtime_cgocall(fn uintptr, arg unsafe.Pointer) int32 // from runtime/cgocall.go
+//go:linkname runtime_libcCall runtime.libcCall
+//go:linkname runtime_entersyscall runtime.entersyscall
+//go:linkname runtime_exitsyscall runtime.exitsyscall
+func runtime_libcCall(fn uintptr, arg unsafe.Pointer) int32 // from runtime/sys_libc.go
+func runtime_entersyscall()                                 // from runtime/proc.go
+func runtime_exitsyscall()                                  // from runtime/proc.go
--- internal/fakecgo/asm.s
+++
@@ -1,6 +0,0 @@
-#include "textflag.h"
-
-GLOBL ·_cgo_thread_start(SB), NOPTR|RODATA, $8
-DATA ·_cgo_thread_start(SB)/8, $·x_cgo_thread_start(SB)
-TEXT ·x_cgo_thread_start(SB), NOSPLIT, $0
-	RET
