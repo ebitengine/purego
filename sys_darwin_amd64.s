21fa41c9129e0d530fe2063905282a827de4bb20
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-07-21 15:13:45 -0400

    update license header to use Ebitengine

--- sys_darwin_amd64.s
+++ sys_darwin_amd64.s
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2022 The Ebiten Authors
+// SPDX-FileCopyrightText: 2022 The Ebitengine Authors
 
 #include "textflag.h"
 #include "internal/abi/abi_amd64.h"
