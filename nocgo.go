9ad04f34e763f0bbc8b7eef2270fef7437bf4afa
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-08-18 13:26:26 -0400

    add comment about not supporting ios in fakecgo

--- nocgo.go
+++ nocgo.go
@@ -1,8 +1,9 @@
-//go:build !cgo
-// +build !cgo
+//go:build !cgo && !ios
+// +build !cgo,!ios
 
 package purego
 
 // if CGO_ENABLED=0 import fakecgo to setup the Cgo runtime correctly.
 // This is required since some frameworks need TLS setup the C way which Go doesn't do.
+// We currently don't support ios in fakecgo mode so force Cgo or fail
 import _ "github.com/ebitengine/purego/internal/fakecgo"
