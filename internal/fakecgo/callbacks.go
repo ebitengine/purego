a6aee6a9acd3c63cc4ec96dd474528fcdb529a08
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-08-18 13:59:05 -0400

    add license and match runtime/cgo version

--- internal/fakecgo/callbacks.go
+++ internal/fakecgo/callbacks.go
@@ -1,6 +1,12 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package fakecgo
 
 import _ "unsafe"
+
+// TODO: decide if we need _runtime_cgo_panic_internal
 
 //go:linkname x_cgo_init_trampoline x_cgo_init_trampoline
 //go:linkname _cgo_init _cgo_init
@@ -11,6 +17,17 @@ var _cgo_init = &x_cgo_init_trampoline
 //go:linkname _cgo_thread_start _cgo_thread_start
 var x_cgo_thread_start_trampoline byte
 var _cgo_thread_start = &x_cgo_thread_start_trampoline
+
+// Creates a new system thread without updating any Go state.
+//
+// This method is invoked during shared library loading to create a new OS
+// thread to perform the runtime initialization. This method is similar to
+// _cgo_sys_thread_start except that it doesn't update any Go state.
+
+//go:linkname x_cgo_sys_thread_create x_cgo_sys_thread_create
+//go:linkname _cgo_sys_thread_create _cgo_sys_thread_create
+var x_cgo_sys_thread_create byte
+var _cgo_sys_thread_create = &x_cgo_sys_thread_create
 
 // Notifies that the runtime has been initialized.
 //
@@ -24,3 +41,6 @@ var _cgo_thread_start = &x_cgo_thread_start_trampoline
 //go:linkname _cgo_notify_runtime_init_done _cgo_notify_runtime_init_done
 var x_cgo_notify_runtime_init_done_trampoline byte
 var _cgo_notify_runtime_init_done = &x_cgo_notify_runtime_init_done_trampoline
+
+// TODO: decide if we need x_cgo_set_context_function
+// TODO: decide if we need _cgo_yield
