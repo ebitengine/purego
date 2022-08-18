2d7813c557cc5b4b1c46a53cc0c84054f2b5a589
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-08-18 13:26:26 -0400

    add comment about not supporting ios in fakecgo

--- cgo.go
+++ cgo.go
@@ -1,8 +1,9 @@
-//go:build cgo
-// +build cgo
+//go:build cgo || ios
+// +build cgo ios
 
 package purego
 
 // if CGO_ENABLED=1 import the Cgo runtime to ensure that it is set up properly.
 // This is required since some frameworks need TLS setup the C way which Go doesn't do.
+// We currently don't support ios in fakecgo mode so force Cgo or fail
 import _ "runtime/cgo"
