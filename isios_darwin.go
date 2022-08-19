b17f2303c739ab01f10802884f47114a0fa78b8c
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-08-19 11:24:41 -0400

    add isios_darwin.go to create a nicer error when build with CGO_ENABLED=0 for GOOS=ios

---
+++ isios_darwin.go
@@ -0,0 +1,11 @@
+//go:build !cgo && ios
+// +build !cgo,ios
+
+package purego
+
+// if you are getting this error it means that you have
+// CGO_ENABLED=0 while trying to build for ios.
+// purego does not support this mode yet.
+// the fix is to set CGO_ENABLED=1 which will require
+// a C compiler.
+var _ = PUREGO_REQUIRES_CGO_ON_IOS
