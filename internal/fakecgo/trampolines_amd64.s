fbb67b618efc10357d13d0e9e9edacd0a52f1866
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-08-19 09:13:20 -0400

    document trampoline reason

--- internal/fakecgo/trampolines_amd64.s
+++ internal/fakecgo/trampolines_amd64.s
@@ -24,6 +24,8 @@
 #include "textflag.h"
 #include "go_asm.h"
 
+// these trampolines map the gcc ABI to Go ABI and then calls into the Go equivalent functions.
+
 // TODO: put <> to make these private
 TEXT x_cgo_init_trampoline(SB), NOSPLIT, $16
 	MOVQ DI, 0(SP)
