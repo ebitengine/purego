aeed57cda74433391c1ebcd6c8ce5630cf944056
Hajime Hoshi <hajimehoshi@gmail.com>
2022-09-05 07:56:23 +0000

    internal/abi: add a dummy Go file for go mod vendor (#32)
    Before this change, the package `internal/abi` was ignored by `go mod vendor`
    as there was no Go file there. Then building purego failed as `internal/abi`
    didn't exist under the `vendor` directory.
    
    This change fixes this issue by adding a dummy Go file to `internal/abi`
    and adding an explicit import to it, but with a `dummy` build tag.
    
    I referred a change https://github.com/goccy/go-graphviz/pull/37 for this
    hack.
    
    Updates #31

---
+++ internal/abi/dummy.go
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022 The Ebitengine Authors
+
+//go:build dummy
+// +build dummy
+
+// Package abi is a dummy package that prevents go tooling from stripping the C dependencies.
+package abi
