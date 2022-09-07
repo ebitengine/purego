195af80a86efedd2e7de0e9ecf2d15b40b8de525
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-09-07 11:27:25 -0400

    linux support

--- dlfcn_linux.go
+++ dlfcn_linux.go
@@ -5,8 +5,5 @@ package purego
 
 //go:cgo_import_dynamic _dlopen dlopen "libdl.so.2"
 //go:cgo_import_dynamic _dlsym dlsym "libdl.so.2"
-
-// on amd64 we don't need the following line - on 386 we do...
-// anyway - with those lines the output is better (but doesn't matter) - without it on amd64 we get multiple DT_NEEDED with "libc.so.6" etc
-
-//go:cgo_import_dynamic _ _ "libdl.so.2"
+//go:cgo_import_dynamic _dlerror dlerror "libdl.so.2"
+//go:cgo_import_dynamic _dlclose dlclose "libdl.so.2"
