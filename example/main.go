5383fbd227a25872d702d7e018d996096621e0a9
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-09-09 18:39:39 -0400

    example

---
+++ example/main.go
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022 The Ebitengine Authors
+
+//go:build example
+// +build example
+
+package main
+
+import (
+	_ "github.com/ebitengine/purego"
+)
+
+func main() {
+}
