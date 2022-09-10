e8e2e45217f385595bfde48c47b86a0fe2cbd2cd
Hajime Hoshi <hajimehoshi@gmail.com>
2022-09-10 03:58:08 +0000

    purego: skip importing `runtime/cgo` for Windows (#41)
    This change resolves these issues:
    
    * Buiding purego for `GOOS=darwin` and `GOARCH=arm64` with Go 1.15
      is not supported so far (#37). Skip this.
    * Buiding purego for `GOOS=windows` with Cgo failed due to duplicated
      symbols (#39). Let's not import `runtime/cgo` for Windows. `fakecgo` is
      implemented only for Darwin so this should be fine. As a side effect, purego
      can no longer be imported from non-Darwin or non-Windows so far.
    
    Updates #37
    Closes #39

---
+++ example/main.go
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022 The Ebitengine Authors
+
+//go:build darwin || windows
+// +build darwin windows
+
+package main
+
+import (
+	_ "github.com/ebitengine/purego"
+)
+
+func main() {
+	// TODO: Add an implementation. This example exists only for testing so far.
+}
