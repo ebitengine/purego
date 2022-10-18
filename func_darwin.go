0256b3ce43ad0abd400e5e8f6b3fce849bd4a68c
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-10-17 22:29:52 -0400

    support reflect.Func and reflect.UnsafePointer argument

--- func_darwin.go
+++ func_darwin.go
@@ -36,8 +36,10 @@ func Func(handle uintptr, name string, fptr interface{}) error {
 				sysargs[i] = uintptr(v.Uint())
 			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 				sysargs[i] = uintptr(v.Int())
-			case reflect.Ptr:
+			case reflect.Ptr, reflect.UnsafePointer:
 				sysargs[i] = v.Pointer() // TODO: keep alive
+			case reflect.Func:
+				sysargs[i] = NewCallback(v.Interface())
 			default:
 				panic("purego: unsupported kind: " + v.Kind().String())
 			}
