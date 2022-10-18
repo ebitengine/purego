d284cd574dc27afc5e2c58e52a3296b827e7afa6
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-10-18 08:40:19 -0400

    remove error return

--- func_darwin.go
+++ func_darwin.go
@@ -4,7 +4,6 @@
 package purego
 
 import (
-	"errors"
 	"reflect"
 	"unsafe"
 
@@ -15,20 +14,20 @@ import (
 // shared object and a pointer to a function representing the calling convention of the C function.
 // fptr will be set to a function that when called will call the C function given by name with the
 // parameters passed in the correct registers and stack.
-// An error is produced if the name symbol cannot be found in handle or if the type is not a function
+// An panic is produced if the name symbol cannot be found in handle or if the type is not a function
 // pointer or if the function returns more than 1 value.
-func Func(handle uintptr, name string, fptr interface{}) error {
+func Func(handle uintptr, name string, fptr interface{}) {
 	sym := Dlsym(handle, name)
 	if sym == 0 {
-		return errors.New("purego: couldn't find symbol" + Dlerror())
+		panic("purego: couldn't find symbol" + Dlerror())
 	}
 	fn := reflect.ValueOf(fptr).Elem()
 	ty := fn.Type()
 	if ty.Kind() != reflect.Func {
-		return errors.New("purego: fptr must be a function pointer")
+		panic("purego: fptr must be a function pointer")
 	}
 	if ty.NumOut() > 1 {
-		return errors.New("purego: function can only return zero or one values")
+		panic("purego: function can only return zero or one values")
 	}
 	v := reflect.MakeFunc(ty, func(args []reflect.Value) (results []reflect.Value) {
 		var sysargs = make([]uintptr, len(args))
@@ -78,5 +77,4 @@ func Func(handle uintptr, name string, fptr interface{}) error {
 		return []reflect.Value{v}
 	})
 	fn.Set(v)
-	return nil
 }
