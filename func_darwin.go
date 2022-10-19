7128aea44c774c29b29ab5e079938f36d88264c6
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-10-19 11:39:18 -0400

    purego: new mechanism for calling C functions (#49)
    This commit adds purego.RegisterLibFunc and purego.RegisterFunc which combines C functions with their Go counterpart. This is a better way to call into C because it is type safe from the caller's perspective. It will also eventually allow for float and struct arguments and returns.

---
+++ func_darwin.go
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022 The Ebitengine Authors
+
+package purego
+
+import (
+	"reflect"
+	"runtime"
+	"unsafe"
+
+	"github.com/ebitengine/purego/internal/strings"
+)
+
+// RegisterLibFunc is a wrapper around RegisterFunc that uses the C function returned from Dlsym(handle, name).
+// It panics if Dlsym fails.
+func RegisterLibFunc(fptr interface{}, handle uintptr, name string) {
+	sym := Dlsym(handle, name)
+	if sym == 0 {
+		panic("purego: couldn't find symbol: " + Dlerror())
+	}
+	RegisterFunc(fptr, sym)
+}
+
+// RegisterFunc takes a pointer to a Go function representing the calling convention of the C function.
+// fptr will be set to a function that when called will call the C function given by cfn with the
+// parameters passed in the correct registers and stack.
+//
+// A panic is produced if the type is not a function pointer or if the function returns more than 1 value.
+//
+// These conversions describe how a Go type in the fptr will be used to call
+// the C function. It is important to note that there is no way to verify that fptr
+// matches the C function. This also holds true for struct types where the padding
+// needs to be ensured to match that of C; RegisterFunc does not verify this.
+//
+// Type Conversions (Go => C)
+//
+//	string <=> char*
+//	bool <=> _Bool
+//	uintptr <=> uintptr_t
+//	uint <=> uint32_t or uint64_t
+//	uint8 <=> uint8_t
+//	uint16 <=> uint16_t
+//	uint32 <=> uint32_t
+//	uint64 <=> uint64_t
+//	int <=> int32_t or int64_t
+//	int8 <=> int8_t
+//	int16 <=> int16_t
+//	int32 <=> int32_t
+//	int64 <=> int64_t
+//	float32 <=> float (WIP)
+//	float64 <=> double (WIP)
+//	struct <=> struct (WIP)
+//	func <=> C function
+//	unsafe.Pointer, *T <=> void*
+//	[]T => void*
+//
+// There is a special case when the last argument of fptr is a variadic interface (or []interface}
+// it will be expanded into a call to the C function as if it had the arguments in that slice.
+// This means that using arg ...interface{} is like a cast to the function with the arguments inside arg.
+// This is not the same as C variadic.
+func RegisterFunc(fptr interface{}, cfn uintptr) {
+	fn := reflect.ValueOf(fptr).Elem()
+	ty := fn.Type()
+	if ty.Kind() != reflect.Func {
+		panic("purego: fptr must be a function pointer")
+	}
+	if ty.NumOut() > 1 {
+		panic("purego: function can only return zero or one values")
+	}
+	v := reflect.MakeFunc(ty, func(args []reflect.Value) (results []reflect.Value) {
+		if len(args) > 0 {
+			if variadic, ok := args[len(args)-1].Interface().([]interface{}); ok {
+				// subtract one from args bc the last argument in args is []interface{}
+				// which we are currently expanding
+				tmp := make([]reflect.Value, len(args)-1+len(variadic))
+				n := copy(tmp, args[:len(args)-1])
+				for i, v := range variadic {
+					tmp[n+i] = reflect.ValueOf(v)
+				}
+				args = tmp
+			}
+		}
+		var sysargs = make([]uintptr, len(args))
+		var keepAlive []interface{}
+		defer func() {
+			runtime.KeepAlive(keepAlive)
+		}()
+		for i, v := range args {
+			switch v.Kind() {
+			case reflect.String:
+				ptr := strings.CString(v.String())
+				keepAlive = append(keepAlive, ptr)
+				sysargs[i] = uintptr(unsafe.Pointer(ptr))
+			case reflect.Uintptr, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+				sysargs[i] = uintptr(v.Uint())
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				sysargs[i] = uintptr(v.Int())
+			case reflect.Ptr, reflect.UnsafePointer, reflect.Slice:
+				keepAlive = append(keepAlive, v.Pointer())
+				sysargs[i] = v.Pointer()
+			case reflect.Func:
+				sysargs[i] = NewCallback(v.Interface())
+			case reflect.Bool:
+				if v.Bool() {
+					sysargs[i] = 1
+				} else {
+					sysargs[i] = 0
+				}
+			default:
+				panic("purego: unsupported kind: " + v.Kind().String())
+			}
+		}
+		r1, _, _ := SyscallN(cfn, sysargs...) //TODO: handle float32/64 and struct types
+		if ty.NumOut() == 0 {
+			return nil
+		}
+		outType := ty.Out(0)
+		v := reflect.New(outType).Elem()
+		switch outType.Kind() {
+		case reflect.Uintptr, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			v.SetUint(uint64(r1))
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			v.SetInt(int64(r1))
+		case reflect.Bool:
+			v.SetBool(r1 != 0)
+		case reflect.UnsafePointer:
+			// We take the address and then dereference it to trick go vet from creating a possible miss-use of unsafe.Pointer
+			v.SetPointer(*(*unsafe.Pointer)(unsafe.Pointer(&r1)))
+		case reflect.Ptr:
+			v = reflect.NewAt(outType, unsafe.Pointer(&r1)).Elem()
+		case reflect.Func:
+			// wrap this C function in a nicely typed Go function
+			v = reflect.New(outType)
+			RegisterFunc(v.Interface(), r1)
+		case reflect.String:
+			v.SetString(strings.GoString(r1))
+		default:
+			panic("purego: unsupported return kind: " + outType.Kind().String())
+		}
+		return []reflect.Value{v}
+	})
+	fn.Set(v)
+}
--- internal/strings/strings.go
+++ internal/strings/strings.go
@@ -3,7 +3,10 @@
 
 package strings
 
-import "unsafe"
+import (
+	"reflect"
+	"unsafe"
+)
 
 // hasSuffix tests whether the string s ends with suffix.
 func hasSuffix(s, suffix string) bool {
@@ -18,4 +21,30 @@ func CString(name string) *byte {
 	var b = make([]byte, len(name)+1)
 	copy(b, name)
 	return &b[0]
+}
+
+// GoString copies a char* to a Go string.
+func GoString(c uintptr) string {
+	// We take the address and then dereference it to trick go vet from creating a possible misuse of unsafe.Pointer
+	ptr := *(*unsafe.Pointer)(unsafe.Pointer(&c))
+	if ptr == nil {
+		return ""
+	}
+	var length int
+	for {
+		// use unsafe.Add once we reach 1.17
+		if *(*byte)(unsafe.Pointer(uintptr(ptr) + uintptr(length))) == '\x00' {
+			break
+		}
+		length++
+	}
+	// use unsafe.Slice once we reach 1.17
+	s := make([]byte, length)
+	var src []byte
+	h := (*reflect.SliceHeader)(unsafe.Pointer(&src))
+	h.Data = uintptr(ptr)
+	h.Len = length
+	h.Cap = length
+	copy(s, src)
+	return string(s)
 }
