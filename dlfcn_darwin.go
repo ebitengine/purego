ec7a70a975491a130b940b79c9b41e66bcaba65f
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-10-21 13:39:19 -0400

    rename libc_* to purego_* (#50)
    While porting purego to Linux I discovered that I had missed the cgo_import_dynamic comment for pthread_sigmask. The reason purego was working was because the symbol is normally linked to in the runtime. I decided to make a unique name for the imported symbol so that we aren't relying on runtime behavior that can easily change.

--- dlfcn_darwin.go
+++ dlfcn_darwin.go
@@ -3,7 +3,7 @@
 
 package purego
 
-//go:cgo_import_dynamic _dlopen dlopen "/usr/lib/libSystem.B.dylib"
-//go:cgo_import_dynamic _dlsym dlsym "/usr/lib/libSystem.B.dylib"
-//go:cgo_import_dynamic _dlerror dlerror "/usr/lib/libSystem.B.dylib"
-//go:cgo_import_dynamic _dlclose dlclose "/usr/lib/libSystem.B.dylib"
+//go:cgo_import_dynamic purego_dlopen dlopen "/usr/lib/libSystem.B.dylib"
+//go:cgo_import_dynamic purego_dlsym dlsym "/usr/lib/libSystem.B.dylib"
+//go:cgo_import_dynamic purego_dlerror dlerror "/usr/lib/libSystem.B.dylib"
+//go:cgo_import_dynamic purego_dlclose dlclose "/usr/lib/libSystem.B.dylib"
