911a84959106c898f1b0b9e49f4ee785dcf9dc4e
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-10-23 13:53:16 -0400

    remove darwin specific constants

--- dlfcn_darwin.go
+++ dlfcn_darwin.go
@@ -13,16 +13,6 @@ const (
 	RTLD_GLOBAL = 0x8 // All symbols are available for relocation processing of other modules.
 )
 
-/*
- * Special handle arguments for dlsym().
- */
-const (
-	RTLD_NEXT      = uintptr(0x8000000000000001) /* Search subsequent objects. */
-	RTLD_DEFAULT   = uintptr(0x8000000000000002) /* Use default search algorithm. */
-	RTLD_SELF      = uintptr(0x8000000000000003) /* Search this and subsequent objects (Mac OS X 10.5 and later) */
-	RTLD_MAIN_ONLY = uintptr(0x8000000000000005) /* Search main executable only (Mac OS X 10.5 and later) */
-)
-
 //go:cgo_import_dynamic purego_dlopen dlopen "/usr/lib/libSystem.B.dylib"
 //go:cgo_import_dynamic purego_dlsym dlsym "/usr/lib/libSystem.B.dylib"
 //go:cgo_import_dynamic purego_dlerror dlerror "/usr/lib/libSystem.B.dylib"
