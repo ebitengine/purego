32c41de34c314a1fd33713dcccbee4efc57a7f44
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-10-23 14:26:48 -0400

    dlfcn constants for linux

--- dlfcn_linux.go
+++ dlfcn_linux.go
@@ -3,6 +3,15 @@
 
 package purego
 
+// Source for constants: https://codebrowser.dev/glibc/glibc/bits/dlfcn.h.html
+
+const (
+	RTLD_LAZY   = 0x00001 // Relocations are performed at an implementation-dependent time.
+	RTLD_NOW    = 0x00002 // Relocations are performed when the object is loaded.
+	RTLD_LOCAL  = 0x00000 // All symbols are not made available for relocation processing by other modules.
+	RTLD_GLOBAL = 0x00100 // All symbols are available for relocation processing of other modules.
+)
+
 //go:cgo_import_dynamic purego_dlopen dlopen "libdl.so.2"
 //go:cgo_import_dynamic purego_dlsym dlsym "libdl.so.2"
 //go:cgo_import_dynamic purego_dlerror dlerror "libdl.so.2"
