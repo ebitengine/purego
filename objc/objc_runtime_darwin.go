fbeb60f11c9695c15c8d6a15a5b22bc185161b80
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-10-30 23:44:28 -0400

    objc: remove unsafe usage from test (#57)
    Add support for NewIMP to take a pointer to an Objective-C struct. This allows the removal of all uses of package unsafe from the tests by creating a struct that matches the structure of the Objective-C object.

--- objc/objc_runtime_darwin.go
+++ objc/objc_runtime_darwin.go
@@ -168,8 +168,10 @@ func GetProtocol(name string) *Protocol {
 // IMP is a function pointer that can be called by Objective-C code.
 type IMP uintptr
 
-// NewIMP takes a Go function that takes (ID, SEL) as its first two arguments. It returns an IMP function
-// pointer that can be called by Objective-C code. The function pointer is never deallocated.
+// NewIMP takes a Go function that takes (ID, SEL) as its first two arguments.
+// ID may instead be a pointer to a struct whose first field has type Class.
+// It returns an IMP function pointer that can be called by Objective-C code.
+// The function pointer is never deallocated.
 func NewIMP(fn interface{}) IMP {
 	ty := reflect.TypeOf(fn)
 	if ty.Kind() != reflect.Func {
@@ -180,7 +182,11 @@ func NewIMP(fn interface{}) IMP {
 	switch {
 	case ty.NumIn() < 2:
 		fallthrough
-	case ty.In(0).Kind() != reflect.Uintptr:
+	case ty.In(0).Kind() != reflect.Uintptr && // checks if it's objc.ID
+		// or that it's a pointer to a struct
+		(ty.In(0).Kind() != reflect.Pointer || ty.In(0).Elem().Kind() != reflect.Struct ||
+			// and that the structs first field is an objc.Class
+			ty.In(0).Elem().Field(0).Type != reflect.TypeOf(Class(0))):
 		fallthrough
 	case ty.In(1).Kind() != reflect.Uintptr:
 		panic("objc: NewIMP must take a (id, SEL) as its first two arguments")
