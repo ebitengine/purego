ca344c56b4bf595cdbfe458fa6eb312e037107c7
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-11-04 09:21:45 -0400

    update example to use globals

--- example/objc/main_darwin.go
+++ example/objc/main_darwin.go
@@ -10,8 +10,15 @@ import (
 	"github.com/ebitengine/purego/objc"
 )
 
+var (
+	sel_new    = objc.RegisterName("new")
+	sel_init   = objc.RegisterName("init")
+	sel_setBar = objc.RegisterName("setBar:")
+	sel_bar    = objc.RegisterName("bar")
+)
+
 type barObject struct {
-	isa objc.Class `objc:"BarObject:NSObject <NSDelegateWindow>"`
+	isa objc.Class `objc:"BarObject : NSObject <NSDelegateWindow>"`
 	bar int
 }
 
@@ -30,11 +37,11 @@ func (b *barObject) SetBar(_cmd objc.SEL, bar int) {
 func (_ *barObject) Selector(metName string) objc.SEL {
 	switch metName {
 	case "Init":
-		return objc.RegisterName("init")
+		return sel_init
 	case "SetBar":
-		return objc.RegisterName("setBar:")
+		return sel_setBar
 	case "Bar":
-		return objc.RegisterName("bar")
+		return sel_bar
 	default:
 		return 0
 	}
@@ -46,9 +53,9 @@ func main() {
 		panic(err)
 	}
 
-	var object = objc.ID(class).Send(objc.RegisterName("new"))
-	object.Send(objc.RegisterName("setBar:"), 123)
-	var bar = int(object.Send(objc.RegisterName("bar")))
+	var object = objc.ID(class).Send(sel_new)
+	object.Send(sel_setBar, 123)
+	var bar = int(object.Send(sel_bar))
 	fmt.Println(bar)
 	// Output: 123
 }
