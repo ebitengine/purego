7f8a369eddfb16edaeb04811ddfa3cd04902417b
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-11-07 17:39:39 -0500

    fix errno

--- internal/unknown/syscall_unknown_linux.go
+++ internal/unknown/syscall_unknown_linux.go
@@ -10,8 +10,10 @@ package unknown
 
 #include <stdint.h>
 #include <dlfcn.h>
-uintptr_t syscall9(uintptr_t fn, uintptr_t a1, uintptr_t a2, uintptr_t a3, uintptr_t a4, uintptr_t a5, uintptr_t a6, uintptr_t a7, uintptr_t a8, uintptr_t a9) {
-	uintptr_t (*func_name)(uintptr_t a1, uintptr_t a2, uintptr_t a3, uintptr_t a4, uintptr_t a5, uintptr_t a6, uintptr_t a7, uintptr_t a8, uintptr_t a9, uintptr_t *errnum);
+#include <errno.h>
+
+uintptr_t syscall9(uintptr_t fn, uintptr_t a1, uintptr_t a2, uintptr_t a3, uintptr_t a4, uintptr_t a5, uintptr_t a6, uintptr_t a7, uintptr_t a8, uintptr_t a9, uintptr_t *errnum) {
+	uintptr_t (*func_name)(uintptr_t a1, uintptr_t a2, uintptr_t a3, uintptr_t a4, uintptr_t a5, uintptr_t a6, uintptr_t a7, uintptr_t a8, uintptr_t a9);
 	*(void**)(&func_name) = (void*)(fn);
 	uintptr_t r1 =  func_name(a1,a2,a3,a4,a5,a6,a7,a8,a9);
 	*errnum = errno;
@@ -23,8 +25,9 @@ import "C"
 
 //go:nosplit
 func Syscall9X(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9 uintptr) (r1, r2, err uintptr) {
+	var errno C.uintptr_t
 	r1 = uintptr(C.syscall9(C.uintptr_t(fn), C.uintptr_t(a1), C.uintptr_t(a2), C.uintptr_t(a3),
 		C.uintptr_t(a4), C.uintptr_t(a5), C.uintptr_t(a6),
-		C.uintptr_t(a7), C.uintptr_t(a8), C.uintptr_t(a9)), &err)
-	return r1, 0, err
+		C.uintptr_t(a7), C.uintptr_t(a8), C.uintptr_t(a9), &errno))
+	return r1, 0, uintptr(errno)
 }
