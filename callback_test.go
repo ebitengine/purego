e10a072b61537e7b63f1e9f6dd24fe5ca4c63b35
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2023-11-28 08:08:37 -0500

    float test for increase argument count

--- callback_test.go
+++ callback_test.go
@@ -127,17 +127,17 @@ func TestNewCallbackFloat32AndFloat64(t *testing.T) {
 	// This tests that calling a function with a mix of float32 and float64 arguments works
 	const (
 		expectedCbTotalF32 = float32(30)
-		expectedCbTotalF64 = float64(15)
+		expectedCbTotalF64 = float64(48)
 	)
 	var cbTotalF32 float32
 	var cbTotalF64 float64
-	imp := purego.NewCallback(func(f1, f2, f3 float32, f4, f5, f6 float64, f7, f8, f9 float32) {
+	imp := purego.NewCallback(func(f1, f2, f3 float32, f4, f5, f6 float64, f7, f8, f9 float32, f10, f11, f12 float64) {
 		cbTotalF32 = f1 + f2 + f3 + f7 + f8 + f9
-		cbTotalF64 = f4 + f5 + f6
+		cbTotalF64 = f4 + f5 + f6 + f10 + f11 + f12
 	})
-	var fn func(f1, f2, f3 float32, f4, f5, f6 float64, f7, f8, f9 float32)
+	var fn func(f1, f2, f3 float32, f4, f5, f6 float64, f7, f8, f9 float32, f10, f11, f12 float64)
 	purego.RegisterFunc(&fn, imp)
-	fn(1, 2, 3, 4, 5, 6, 7, 8, 9)
+	fn(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 
 	if cbTotalF32 != expectedCbTotalF32 {
 		t.Errorf("cbTotalF32 not correct got %f but wanted %f", cbTotalF32, expectedCbTotalF32)
