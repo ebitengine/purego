d41e7da75c36d31d46a83a02c2d018fd6150299f
Romain Marcadier <romain@datadoghq.com>
2024-01-08 12:07:30 +0100

    make nested.cpp windows-friendly

--- libdlnested/nested.cpp
+++ libdlnested/nested.cpp
@@ -1,13 +1,22 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023 The Ebitengine Authors
 
+#ifdef _WIN32
+#include <libloaderapi.h>
+#else
 #include <dlfcn.h>
+#endif
 
 struct nested_libdl
 {
     nested_libdl() {
-        // Fails for sure because a symbol cannot be named like this 
+#ifdef _WIN32
+        // Fails for sure because a symbol cannot be named like this
+        GetProcAddress(GetModuleHandleA(NULL), "@/*<>");
+#else
+        // Fails for sure because a symbol cannot be named like this
         dlsym(RTLD_DEFAULT, "@/*<>");
+#endif
     }
 };
 
