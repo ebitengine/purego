479d078f4469f53079d44b18309be0d58b072b12
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2024-01-17 10:19:39 -0500

    build test

---
+++ buildtest/main.go
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 The Ebitengine Authors
+
+package main
+
+import (
+	_ "github.com/ebitengine/purego"
+)
+
+import "C"
+
+// This file tests that build Cgo and purego at the same time succeeds to build (#189).
+func main() {
+
+}
