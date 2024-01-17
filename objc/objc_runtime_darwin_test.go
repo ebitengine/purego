c80aed1a8e7d970ad6e442cffb8f1ae1d02dd2c3
Hajime Hoshi <hajimehoshi@gmail.com>
2024-01-18 00:50:03 +0900

    objc: use a fullpath for dlopen
    Closes #198

--- objc/objc_runtime_darwin_test.go
+++ objc/objc_runtime_darwin_test.go
@@ -104,7 +104,7 @@ func ExampleID_SendSuper() {
 
 func TestSend(t *testing.T) {
 	// NSNumber comes from Foundation so make sure we have linked to that framework.
-	_, err := purego.Dlopen("Foundation.framework/Foundation", purego.RTLD_GLOBAL)
+	_, err := purego.Dlopen("/System/Library/Frameworks/Foundation.framework/Foundation", purego.RTLD_GLOBAL)
 	if err != nil {
 		t.Fatal(err)
 	}
