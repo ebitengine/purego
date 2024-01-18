d0091a93debe061a7beccbdc1bb5fbc3c3abb088
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2024-01-18 09:35:48 -0500

    purego: Fix for darwin (#200)
    Closes #201

--- dlfcn_stubs.s
+++ dlfcn_stubs.s
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2022 The Ebitengine Authors
 
-//go:build !cgo && (darwin || freebsd || linux)
+//go:build darwin || !cgo && (freebsd ||  linux)
 
 #include "textflag.h"
 
