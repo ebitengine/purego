76d6e5715c0c2f363b8113c94dfc7e3d7dd98e05
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2024-01-27 10:18:11 -0500

    fix spacing

--- sys_amd64.s
+++ sys_amd64.s
@@ -118,7 +118,7 @@ TEXT callbackasm1(SB), NOSPLIT|NOFRAME, $0
 	MOVQ  R9, (14*8)(SP)
 	LEAQ  8(SP), R8      // R8 = address of args vector
 
-	PUSHQ R10     // push the stack pointer below registers
+	PUSHQ R10 // push the stack pointer below registers
 
 	// determine index into runtime·cbs table
 	MOVQ $callbackasm(SB), DX
@@ -154,7 +154,7 @@ TEXT callbackasm1(SB), NOSPLIT|NOFRAME, $0
 
 	POP_REGS_HOST_TO_ABI0()
 
-    POPQ R10 // get the SP back
+	POPQ  R10        // get the SP back
 	ADJSP $-14*8, SP // remove arguments
 
 	MOVQ R10, 0(SP)
