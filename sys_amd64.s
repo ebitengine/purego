6ff43c50973341be5c8b0518261eca871aba4b7f
Hajime Hoshi <hajimehoshi@gmail.com>
2024-01-28 00:32:18 +0900

    Revert "Align stack to 16 bytes to avoid SIGBUS (#207)"
    This reverts commit 52ff70969eb959965477219e3359f58ed3b4f821.
    
    Reason: test fails on Linux w/ Go 1.22-rc.2:
    
    ```
    === RUN   TestRegisterFunc
    signal: segmentation fault (core dumped)
    FAIL	github.com/ebitengine/purego	0.151s
    FAIL
    ```

--- sys_amd64.s
+++ sys_amd64.s
@@ -74,9 +74,11 @@ TEXT syscall9X(SB), NOSPLIT|NOFRAME, $0
 	RET
 
 TEXT callbackasm1(SB), NOSPLIT|NOFRAME, $0
-	MOVQ 0(SP), AX  // save the return address to calculate the cb index
-	MOVQ 8(SP), R10 // get the return SP so that we can align register args with stack args
-	ADDQ $8, SP     // remove return address from stack, we are not returning to callbackasm, but to its caller.
+	// remove return address from stack, we are not returning to callbackasm, but to its caller.
+	MOVQ 0(SP), AX
+	ADDQ $8, SP
+
+	MOVQ 0(SP), R10 // get the return SP so that we can align register args with stack args
 
 	// make space for first six int and 8 float arguments below the frame
 	ADJSP $14*8, SP
@@ -96,7 +98,7 @@ TEXT callbackasm1(SB), NOSPLIT|NOFRAME, $0
 	MOVQ  R9, (14*8)(SP)
 	LEAQ  8(SP), R8      // R8 = address of args vector
 
-	PUSHQ R10 // push the stack pointer below registers
+	MOVQ R10, 0(SP) // push the stack pointer below registers
 
 	// determine index into runtime·cbs table
 	MOVQ $callbackasm(SB), DX
@@ -132,7 +134,8 @@ TEXT callbackasm1(SB), NOSPLIT|NOFRAME, $0
 
 	POP_REGS_HOST_TO_ABI0()
 
-	POPQ  R10        // get the SP back
+	MOVQ 0(SP), R10 // get the SP back
+
 	ADJSP $-14*8, SP // remove arguments
 
 	MOVQ R10, 0(SP)
