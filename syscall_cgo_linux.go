03b94da9f7684cc1ef67c75785ca5c4124edc614
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2024-02-24 17:50:08 -0500

    forgot other platforms

--- syscall_cgo_linux.go
+++ syscall_cgo_linux.go
@@ -18,6 +18,7 @@ type syscall15Args struct {
 	fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15 uintptr
 	f1, f2, f3, f4, f5, f6, f7, f8                                       uintptr
 	r1, r2, err                                                          uintptr
+	arm64_r8                                                             uintptr
 }
 
 //go:nosplit
