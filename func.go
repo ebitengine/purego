72d4924a22d82f7fce35f58628534fec49dc9be2
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2024-04-15 20:37:12 -0400

    ugh logic hard....

--- func.go
+++ func.go
@@ -117,7 +117,7 @@ func RegisterFunc(fptr interface{}, cfn uintptr) {
 	if cfn == 0 {
 		panic("purego: cfn is nil")
 	}
-	if ty.NumOut() == 1 && ty.Out(0).Kind() == reflect.Float32 || ty.Out(0).Kind() == reflect.Float64 &&
+	if ty.NumOut() == 1 && (ty.Out(0).Kind() == reflect.Float32 || ty.Out(0).Kind() == reflect.Float64) &&
 		runtime.GOARCH != "arm64" && runtime.GOARCH != "amd64" {
 		panic("purego: float returns are not supported")
 	}
