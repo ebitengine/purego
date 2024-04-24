a0ba62c17032bbea9955761ed45c261dede9aab1
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2024-04-24 13:54:09 -0400

    clarification

--- syscall.go
+++ syscall.go
@@ -7,7 +7,7 @@ package purego
 
 // CDecl marks a function as being called using the __cdecl calling convention as defined in
 // the [MSDocs] when passed to NewCallback. It must be the first argument to the function.
-// This is only useful on 386, but it is safe to use on other platforms.
+// This is only useful on 386 Windows, but it is safe to use on other platforms.
 //
 // [MSDocs]: https://learn.microsoft.com/en-us/cpp/cpp/cdecl?view=msvc-170
 type CDecl struct{}
