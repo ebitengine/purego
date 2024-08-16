10556e53f26a40c3f2af7892b90b97d45c8fe687
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2024-08-15 22:31:11 -0400

    CGO support for Netbsd

--- internal/cgo/dlfcn_cgo_unix.go
+++ internal/cgo/dlfcn_cgo_unix.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024 The Ebitengine Authors
 
-//go:build freebsd || linux
+//go:build freebsd || linux || netbsd
 
 package cgo
 
--- internal/cgo/syscall_cgo_unix.go
+++ internal/cgo/syscall_cgo_unix.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2022 The Ebitengine Authors
 
-//go:build freebsd || (linux && !(arm64 || amd64))
+//go:build freebsd || (linux && !(arm64 || amd64)) || netbsd
 
 package cgo
 
