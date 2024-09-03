c3f4c9c274c333112cd6901e5c2be717f517ef89
Hajime Hoshi <hajimehoshi@gmail.com>
2024-09-04 00:08:04 +0900

    examples: remove depdencies on golang.org/x/sys/windows (#278)
    Closes #270

--- examples/libc/main_windows.go
+++ examples/libc/main_windows.go
@@ -3,9 +3,11 @@
 
 package main
 
-import "golang.org/x/sys/windows"
+import "syscall"
 
 func openLibrary(name string) (uintptr, error) {
-	handle, err := windows.LoadLibrary(name)
+	// Use [syscall.LoadLibrary] here to avoid external dependencies (#270).
+	// For actual use cases, [golang.org/x/sys/windows.NewLazySystemDLL] is recommended.
+	handle, err := syscall.LoadLibrary(name)
 	return uintptr(handle), err
 }
