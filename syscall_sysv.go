05d8da40782673669b51533ccbfedfa71dd8c437
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2024-10-18 13:52:02 -0400

    use sync.Pool in syscall_syscall15X (#285)

--- syscall_sysv.go
+++ syscall_sysv.go
@@ -16,12 +16,14 @@ var syscall15XABI0 uintptr
 
 //go:nosplit
 func syscall_syscall15X(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15 uintptr) (r1, r2, err uintptr) {
-	args := syscall15Args{
+	args := thePool.Get().(*syscall15Args)
+	defer thePool.Put(args)
+	*args = syscall15Args{
 		fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15,
 		a1, a2, a3, a4, a5, a6, a7, a8,
 		0,
 	}
-	runtime_cgocall(syscall15XABI0, unsafe.Pointer(&args))
+	runtime_cgocall(syscall15XABI0, unsafe.Pointer(args))
 	return args.a1, args.a2, 0
 }
 
