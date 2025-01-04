be741aae7bef2e978587432e9854badaed16cee3
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2025-01-04 12:32:53 -0500

    fakecgo: C functions require nosplit
    fakecgo runs on systemstack so we shouldn't ever split the stack.
    
    I also updated the size of sigset_t to be the correct size based on the headers. This was needed to make it possible to build with inlining disabled when nosplit was added.
    
    Fixes #287

--- internal/fakecgo/libcgo.go
+++ internal/fakecgo/libcgo.go
@@ -6,8 +6,11 @@
 package fakecgo
 
 type (
-	size_t         uintptr
-	sigset_t       [128]byte
+	size_t uintptr
+	// Sources:
+	// Darwin - https://github.com/apple/darwin-xnu/blob/2ff845c2e033bd0ff64b5b6aa6063a1f8f65aa32/bsd/sys/_types.h#L74
+	// FreeBSD - https://github.com/DoctorWkt/xv6-freebsd/blob/d2a294c2a984baed27676068b15ed9a29b06ab6f/include/signal.h#L98C9-L98C21
+	sigset_t       [32]byte
 	pthread_attr_t [64]byte
 	pthread_t      int
 	pthread_key_t  uint64
