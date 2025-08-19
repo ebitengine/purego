adbec3d5a98fc7b672bce9bb4f0ec1cafee979ab
judah <judah@tuta.io>
2025-08-18 22:14:59 -0600

    .

--- testdata/runcl.bat
+++ testdata/runcl.bat
@@ -1,5 +1,7 @@
 echo off
 
+REM @todo(judah): PATH SHOULD NOT BE HARDCODED, BETTER WAY TO FETCH THIS DYNAMICALLY?
+
 call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat" %1
 
 cl.exe /LD /W1 /WX /Fe:%2 %3 %4 %5 %6 %7 %8 %9
