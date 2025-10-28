793496b371a9b285444fc4bbc2ab1da651152f59
Travis Cline <travis.cline@gmail.com>
2025-10-28 09:45:09 -0700

    tests: fix Darwin integer/string mixing in ABI tests

--- func_test.go
+++ func_test.go
@@ -187,9 +187,11 @@ func TestABI(t *testing.T) {
 	}
 	{
 		const cName = "test_8i32_3strings"
-		var fn func(int32, int32, int32, int32, int32, int32, int32, int32, string, string, string) string
+		var fn func(*byte, uintptr, int32, int32, int32, int32, int32, int32, int32, int32, string, string, string)
 		purego.RegisterLibFunc(&fn, lib, cName)
-		res := fn(1, 2, 3, 4, 5, 6, 7, 8, "foo", "bar", "baz")
+		buf := make([]byte, 256)
+		fn(&buf[0], 256, 1, 2, 3, 4, 5, 6, 7, 8, "foo", "bar", "baz")
+		res := string(buf[:strings.IndexByte(string(buf), 0)])
 		const want = "1:2:3:4:5:6:7:8:foo:bar:baz"
 		if res != want {
 			t.Fatalf("%s: got %q, want %q", cName, res, want)
