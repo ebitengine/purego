d8dcb88add90fa48b3fe17d8f9c861514a9b8f77
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2026-02-14 09:10:04 -0500

    return errno value on darwin amd64/arm64 (#416)
    It adds support to return the errno value on Darwin amd64/arm64. I don't recall why I removed it and it is needed if we want to ever return it as an error type from RegisterFunc calls. This also brings it back in line with he function doc for SyscallN that says it returns the errno but right now it doesn't.
    
    Updates #244
    Updates #390

--- syscall_sysv.go
+++ syscall_sysv.go
@@ -26,7 +26,7 @@ func syscall_syscall15X(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a
 	}
 
 	runtime_cgocall(syscall15XABI0, unsafe.Pointer(args))
-	return args.a1, args.a2, 0
+	return args.a1, args.a2, args.a3
 }
 
 // NewCallback converts a Go function to a function pointer conforming to the C calling convention.
--- syscall_test.go
+++ syscall_test.go
@@ -5,9 +5,12 @@ package purego_test
 
 import (
 	"os"
+	"runtime"
 	"testing"
+	"unsafe"
 
-	_ "github.com/ebitengine/purego"
+	"github.com/ebitengine/purego"
+	"github.com/ebitengine/purego/internal/load"
 )
 
 func TestOS(t *testing.T) {
@@ -19,5 +22,35 @@ func TestOS(t *testing.T) {
 	err = os.Unsetenv("TESTING")
 	if err != nil {
 		t.Errorf("failed to Unsetenv: %s", err)
+	}
+}
+
+func TestErrno(t *testing.T) {
+	if runtime.GOOS != "darwin" {
+		t.Skip("platform does not support returning errno from syscall")
+	}
+
+	libc, err := load.OpenLibrary("/usr/lib/libSystem.B.dylib")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	openSym, err := load.OpenSymbol(libc, "open")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r1, _, errno := purego.SyscallN(openSym, uintptr(unsafe.Pointer(&[]byte("_file_that_does_not_exist_\x00")[0])), uintptr(os.O_RDWR))
+	if int32(r1) != -1 {
+		t.Errorf("open returned %d, wanted -1", r1)
+	}
+
+	var strerror func(int32) string
+	purego.RegisterLibFunc(&strerror, libc, "strerror")
+
+	const expected = "No such file or directory"
+	got := strerror(int32(errno))
+	if got != expected {
+		t.Errorf("strerror returned %q, wanted \"%s\"", got, expected)
 	}
 }
